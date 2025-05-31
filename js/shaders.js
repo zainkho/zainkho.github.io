325ef74bd50404df59bbe8e22935f3dea1464b30
Zain Khoja <zainkhoja7@gmail.com>
2025-05-31 18:31:15 -0400

    Lint with prettier

--- js/shaders.js
+++ js/shaders.js
@@ -1,7 +1,12 @@
 import * as three from 'three';
 
 const scene = new three.Scene();
-const camera = new three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new three.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
 const renderer = new three.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
